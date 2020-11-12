c183881839705c1d1e599602338c34cf23946537
mmossien6 <46699151+mmossien6@users.noreply.github.com>
2020-11-11 22:17:17 -0500

    Add files via upload

--- ttweetcli.py
+++ ttweetcli.py
@@ -1,12 +1,10 @@
 ####################################################################################################################################
 #Information
 ##
-##Name: Cody Kantor
-##GTID: 903412020
-##E-mail: ckantor8@gatech.edu
+##Names: Cody Kantor, Mark Mossien
 ##Class: CS 3251 - Networking I
-##Date: 9/22/2020
-##Assignment: Programming Assignment 1 - Trivial Twitter
+##Date: 11/11/2020
+##Assignment: Programming Assignment 2
 ####################################################################################################################################
 
 ## Necessary imports for socket programming and using terminal arguments
@@ -17,50 +15,50 @@ import threading
 import _thread
 from _thread import *
 
-tl = []
+tl = [] #initialization for sending thread
 
-def invaliduser(string): 
+def invaliduser(string): #given a string for the username, determines whether/not it is legal.
     if string == "":
         return True
     for s in string:
-        val = ord(s)
+        val = ord(s)    #name is invalid if it contains any character not in [A-Z, a-z, 0-9]
         if (val < 48 or (val > 57 and val < 65) or (val > 90 and val < 97) or (val > 122)):
             return True
     return False
 
-def invalidhashtag(string): 
+def invalidhashtag(string):  #given a string for the hashtag, determines whether/not it is legal.
     if (string[0] != "#" or "##" in string or string == "#ALL"):
-        return True
+        return True    #basic cases for validity
 
     for s in string:
-        val = ord(s)
+        val = ord(s)    #tests for invalid characters not in [A-Z, a-z, 0-9]
         if (val < 48 or (val > 57 and val < 65) or (val > 90 and val < 97) or (val > 122)):
             if (val != 35):
                 return True
     return False
 
 
-args = sys.argv ## Retrieve arguments passed to terminal
-numArgs = len(sys.argv) ## Acquire number of arguments passed
+args = sys.argv         #retrieve arguments passed to terminal
+numArgs = len(sys.argv) # Acquire number of arguments passed
 
 ## Verify and Handle Number of Args
-if numArgs < 4: ## Too few arguments
+if numArgs < 4: ## if there are too few arguments
     print("error: args should contain <ServerIP> <ServerPort> <Username>") ## Provide error statement
     exit() ## exit gracefully
 
-if numArgs > 4: ## Too many arguments
+if numArgs > 4: ## if there are too many arguments
     print("error: args should contain <ServerIP> <ServerPort> <Username>") ## Provide error statement
     exit() ## exit gracefully
     
-if (numArgs == 4): ## No message included, so flag should should be -d
+if (numArgs == 4): ## no message included, so flag should should be -d
     params = {"ip": args[1], "port": args[2], "user": args[3],} ## Enumerate arguments as parameters
     if (params["ip"] < "0.0.0.0" or params["ip"] > "255.255.255.255"): ## Check if IP is valid
         print("error: server ip invalid, connection refused.") ## Provide error statement
         exit() ## Exit gracefully
-    if (params["port"] < "1" or params["port"] > "65535"):
-        print("error: server port invalid, connection refused.")
+    if (params["port"] < "1" or params["port"] > "65535"): #check if port is valid
+        print("error: server port invalid, connection refused.") ## Provide error statement
         exit() ## exit gracefully
-    if (invaliduser(params["user"])):
+    if (invaliduser(params["user"])): #checks if username is valid or not
         print("error: username has wrong format, connection refused.")
         exit() ## exit gracefully
 
@@ -73,101 +71,102 @@ except: ## If connection fails, go to exception handler
     print("Error: Server Not Found") ## Provide error statement
     exit() ## Exit gracefully
 
-s.send((params["user"]).encode())
-validUsername = s.recv(1024).decode()
-if (validUsername == "False"):
+s.send((params["user"]).encode()) #after performing all other input checks, send username to server to see if it is already in use.
+validUsername = s.recv(1024).decode() #if it is in use, then the server will determine this and send back "False".
+if (validUsername == "False"):   #let user know it's invalid and gracefully exit
     print("username illegal, connection refused.")
-    exit()
+    exit()   
 
-print("username legal, connection established.")
+print("username legal, connection established.") #otherwise, all good.
 
 def sending():
-    mysubs = []
+    mysubs = [] #for storing a client's subscribed tweets
     
-    
-    while True:
-
-        Input = str(input())
-        if "\"" in Input:
-            cmd = Input.split("\"")
-            cmd = [c.strip() for c in cmd]
+    while True: #repeats until the user wishes to exit
+        Input = str(input()) #gets the user's input command
+        if "\"" in Input: #if there are quotes in the input that means we need to split by quote to properly save the tweet.
+            cmd = Input.split("\"")    #split by quote,
+            cmd = [c.strip() for c in cmd]  #and remove trailing whitespace
         else:
-            cmd = Input.split()
+            cmd = Input.split() #otherwise, if there are no quotes then just split the input up into distinct words
                 
-        t2 = 0
+        t2 = 0 #initialization for receiving thread
         
-        if (cmd[0] == "exit"):
-            s.send((params["user"]+" "+Input).encode())
+        #at this point, cmd[0] contains the main command; i.e. tweet, getusers, etc.
+
+        if (cmd[0] == "exit"): #if the command is exit:
+            s.send((params["user"]+" "+Input).encode()) #send the server a message to inform which user, and that the user is exiting.
             break ## exit gracefully
         
-        t2 = threading.Thread(target=receiving)
-        t2.start()
+        t2 = threading.Thread(target=receiving) #begin the receiving thread, to form other half of send/receive threading per client
+        t2.start()   #begin thread activity
 
-        if (cmd[0] == "tweet"):
-            if (len(cmd[1]) <= 0 and cmd[1] != " "):
-                print("message format illegal")
+        if (cmd[0] == "tweet"): #if command is tweet
+            if (len(cmd[1]) <= 0 and cmd[1] != " "): #make sure the tweet message has a finite length
+                print("message format illegal") #let user know otherwise
                 continue ## exit gracefully
-            if (len(cmd[1]) > 150):
-                print("message length illegal, connection refused.")
+            if (len(cmd[1]) > 150): #make sure the tweet isnt too long
+                print("message length illegal, connection refused.") #let user know if so
                 continue ## exit gracefully
-            if (invalidhashtag(cmd[2]) or cmd[2] == "#ALL"):
-                print("hashtag illegal format, connection refused.")
+            if (invalidhashtag(cmd[2]) or cmd[2] == "#ALL"): #if the hashtag is invalid (based on method above)
+                print("hashtag illegal format, connection refused.") #let user know
                 continue ## exits gracefully
-            s.send((params["user"]+" "+cmd[0] + ': "' + cmd[1] + '" ' + cmd[2]).encode())
-            continue
+            s.send((params["user"]+" "+cmd[0] + ': "' + cmd[1] + '" ' + cmd[2]).encode()) #at this point the tweet is valid, so send it to the server,
+            continue                                                                    #along with the user that it originated from.
         
-        if (cmd[0] == "subscribe"):
-            if(len(mysubs) == 3 or cmd[1] in mysubs):
-                print("operation failed: sub " + cmd[1] + " failed, already exists or exceeds 3 limitation")
+        if (cmd[0] == "subscribe"): #if command is subscribe
+            if(len(mysubs) == 3 or cmd[1] in mysubs): #if already subscribed or too many subscriptions:
+                print("operation failed: sub " + cmd[1] + " failed, already exists or exceeds 3 limitation") #let user know.
                 continue ## exits gracefully
-            mysubs.append(cmd[1])
-            s.send((params["user"] + " " + Input).encode())
+            mysubs.append(cmd[1]) #add the sub to the subs list, since it is valid.
+            s.send((params["user"] + " " + Input).encode()) #send this information to the server.
         
-        if (cmd[0] == "unsubscribe"):
-            if(cmd[1] == "#ALL"):
+        if (cmd[0] == "unsubscribe"): #if command is unsubscribe
+            if(cmd[1] == "#ALL"): #if all, then reset the subs list.
                 mysubs = []
-            for subs in mysubs:
-                if subs == cmd[1]:
-                    mysubs.remove(subs)
-            s.send((params["user"] + " " + Input).encode())
+            for subs in mysubs: #if not all, then search through the subs list to find correct one to
+                if subs == cmd[1]:              #unsubscribe to
+                    mysubs.remove(subs) #remove it
+            s.send((params["user"] + " " + Input).encode()) #send this information to the server.
         
-        if (cmd[0] == "getusers"):
-            s.send((params["user"]+" "+Input).encode())
+        if (cmd[0] == "getusers"): #if command is getusers
+            s.send((params["user"]+" "+Input).encode()) #send this information to the server.
             
-        if (cmd[0] == "gettweets"):
-            s.send(Input.encode())
+        if (cmd[0] == "gettweets"):#if command is gettweets
+            s.send(Input.encode()) #send this information to the server.
         
-        if (cmd[0] == "timeline"):
-            s.send((params["user"]+" "+Input).encode())
-            
-    print("bye bye")
-    s.close()
-    sys.exit()  #shouting
+        if (cmd[0] == "timeline"): #if command is timeline
+            s.send((params["user"]+" "+Input).encode()) #send this information to the server
+        #repeat back until the user is done.
 
-def receiving(): #listening
+    print("bye bye")
+    s.close() #close the current client connection, and exit.
+    sys.exit() 
+
+def receiving(): #code for the receiving thread for the current client
     while True:
-        try:
-            if s:
-                res = s.recv(1024)
-                res = res.decode()
-                if (": " in res and "*" not in res):
-                    tl.append(res)
-                if len(res) == 0:
+        try: #the socket might have been closed, in which case jump to the except and exit.
+            if s: #if the socket still exists while listening (aka the connection is still open)
+                res = s.recv(1024) #accept data from the server for parsing
+                res = res.decode() 
+                if (": " in res and "*" not in res): 
+                    tl.append(res) #for gettweets; add the tweetlist for display.
+                if len(res) == 0: #ignore if no data
                     pass
                 if "*" in res:
                     res = res.strip("*")
                 if (res == "Timeline:"):
-                    for post in tl:
+                    for post in tl: #if timeline, then display everything in t1,
                         print(post)
-                else:
+                else: #otherwise just print the tweetlist.
                     print(res)
         except:
-            sys.exit()
+            sys.exit() #if the socket is closed, then exit the program.
 
 
 
-t1 = threading.Thread(target=sending)
-t1.start()
+t1 = threading.Thread(target=sending) #initializes and starts the sending thread for the calling client.
+t1.start()    #each client will have two threads associated with it.
 
 
 
--- ttweetser.py
+++ ttweetser.py
@@ -1,12 +1,10 @@
 ####################################################################################################################################
 #Information
 ##
-##Name: Cody Kantor
-##GTID: 903412020
-##E-mail: ckantor8@gatech.edu
+##Names: Cody Kantor, Mark Mossien
 ##Class: CS 3251 - Networking I
-##Date: 9/22/2020
-##Assignment: Programming Assignment 1 - Trivial Twitter
+##Date: 11/11/2020
+##Assignment: Programming Assignment 2
 ####################################################################################################################################
 
 ## Necessary imports for socket programming and using terminal arguments
@@ -30,117 +28,116 @@ print("Waiting for connection...") ## Print waiting for connection message to up
 s.listen(5) ## Set socket to listen for connection
 
 tweets = []
-subs = []
+subs = [] #initialize lists for tweets, subs and users
 users = []
-ThreadCount = 0          ## Initialize ThreadCount
+ThreadCount = 0  ## Initialize ThreadCount
 
-def multi_threaded_client(connection):
-    while True:
-        data = connection.recv(2048)
+def multi_threaded_client(connection): #function for each new thread
+    while True: #repeat indefinitely:
+        data = connection.recv(2048) #receive data from client
         unsplitdata = data.decode()
         if not unsplitdata:
             break
                 
-        if "\"" in unsplitdata:
+        if "\"" in unsplitdata: 
             data = unsplitdata.split("\"")
             data = [d.strip() for d in data]
             data0 = data[0].split()[0]
-            data1 = data[0].split()[1]
-            data.append(data[2])
-            origintag = data[3]
-            data[3] = data[3].split("#")
+            data1 = data[0].split()[1]         #this section parses the code into a list of data[] so that
+            data.append(data[2])               #the client's commands can be more easily parsed later in the function.
+            origintag = data[3]                #only run this section if the unparsed data has quotes; so, essentially,
+            data[3] = data[3].split("#")       #only when the client's command is a tweet.
             data[3].remove("")
             data[2] = data[1]
             data[1] = data1
             data[0] = data0
         else:
-            data = unsplitdata.split()
+            data = unsplitdata.split() #if not tweet, then just split by space.
         
         data[1] = data[1].strip(":")
         
-        if (data[1] == "timeline"):
-            connection.send(("Timeline:").encode())
+        if (data[1] == "timeline"): #if the command is timeline,
+            connection.send(("Timeline:").encode()) #send this info to the client, and let client handle.
 
-        if (data[1] == "getusers"):
+        if (data[1] == "getusers"): #if the command is getusers,
             userlist = ""
-            for user in users:
+            for user in users: #generate a user list from the server userlist.
                 userlist = userlist + user + "\n"
-            connection.send(userlist.strip().encode())
+            connection.send(userlist.strip().encode()) #send this info to the client.
         
-        if(data[1] == "exit"):
-            for tweet in tweets:
+        if(data[1] == "exit"): #if the command is exit, then clear out the server lists.
+            for tweet in tweets: #clear the user's tweets,
                 if data[0] == tweet.get("user"):
                     tweets.remove(tweet)
-            for sub in subs:
+            for sub in subs:     #clear the user's subs,
                 if data[0] == sub.get("user"):
                     subs.remove(sub)
-            for user in users:
+            for user in users:   #and clear the user.
                 if data[0] == user:
                     users.remove(user)
             break
         
-        if (data[1] == "tweet"):
-            tweets.append({'user': data[0], 'msg': data[2], 'tag': origintag})
+        if (data[1] == "tweet"): #if the command is tweet
+            tweets.append({'user': data[0], 'msg': data[2], 'tag': origintag}) #save the tweet
             subber = ""
-            for sub in subs:
+            for sub in subs: #search through the list of subs to see which clients to push the tweet to.
                 if ((sub.get("tag") == "ALL" or sub.get("tag") in data[3]) and subber != sub.get("user")):
-                    subber = sub.get("user")
+                    subber = sub.get("user")     #push the tweet to the client if they are subbed to the hashtag.
                     sub.get('client').send((data[0]+": "+'"'+data[2]+'" '+origintag).encode())
-            ##connection.send(("tweet operation success").encode())
         
-        if (data[1] == "subscribe"):
-            subs.append({'user': data[0], 'tag': data[2].strip("#"), 'client': connection})
-            connection.send(("operation success").encode())
+        if (data[1] == "subscribe"): #if the command is subscribe
+            subs.append({'user': data[0], 'tag': data[2].strip("#"), 'client': connection}) #add the hashtag to that user's list of subs.
+            connection.send(("operation success").encode()) #then send success message to the client
         
-        if (data[1] == "unsubscribe"):
-            if (data[2] == "#ALL"):
+        if (data[1] == "unsubscribe"): #if the command is unsubscribe
+            if (data[2] == "#ALL"): #if unsubscribing from all hashtags,
                 for sub in subs:
-                    if (data[0] == sub.get("user")):
+                    if (data[0] == sub.get("user")): #then remove all of the subs for that user.
                         subs.remove(sub)
                 for sub in subs:
                     if (data[0] == sub.get("user")):
                         subs.remove(sub)
-            else:
-                for sub in subs:
+            else: 
+                for sub in subs: #otherwise, find the specific sub for that user, and unsubscribe from just that hashtag.
                     if ((data[2].strip("#") == sub.get("tag") or data[2].strip("#") == "ALL") and data[0] == sub.get("user")):
                         subs.remove(sub)
-            connection.send(("operation success").encode())
+            connection.send(("operation success").encode()) #then send success message to the client
         
-        if (data[0] == "gettweets"):
+        if (data[0] == "gettweets"): #if command is gettweets
             tweetlist = ""
-            for tweet in tweets:
+            for tweet in tweets: #find all of the corresponding tweets to the called user, and add them to a list.
                 if data[1] == tweet.get("user"):
                     tweetlist = tweetlist+tweet.get("user")+': "'+tweet.get("msg")+'" '+tweet.get("tag")+"\n"
 
-            if not (data[1] in users):
-                connection.send(("no user "+data[1]+" in the system").encode())
-            else:
+            if not (data[1] in users): #if the user isn't found in the userlist, then there is nothing to display.
+                connection.send(("no user "+data[1]+" in the system").encode()) #let user know
+            else: #otherwise, send the user the tweet list for the desired user.
                 connection.send((tweetlist.strip()+"*").encode())
         
-    connection.close()
+    connection.close() #after the function exists, close the connection.
 
 while True: ## while True will run forever without crashing
     conn, addr = s.accept() ## Accept socket connection
     print('Connected by', addr) ## Print connection information
     
-    name = conn.recv(1024)
+    name = conn.recv(1024) #receive username information from the client
     name = name.decode()
     validName = True
     for u in users:
-    	if u == name:
-    		validName = False             #duplicate username check
-    		break
+        if u == name:   #makes sure that the desired username is not already in use
+            validName = False  #duplicate username check
+            break
 
-    if (validName):
-    	users.append(name)
-    	#print("successfully accepted user", name)
-    	conn.send("True".encode())
-    	start_new_thread(multi_threaded_client, (conn, ))
-    	ThreadCount = ThreadCount + 1
-    else:
-    	conn.send("False".encode())
-    	conn.close()
-s.close()
+    if (validName): #if the username is valid, then add to the user list and begin a new thread for the corresponding client.
+        users.append(name)
+        #print("successfully accepted user", name)
+        conn.send("True".encode()) #lets client know that the username was valid
+        start_new_thread(multi_threaded_client, (conn, )) #starts a new thread for the client
+        ThreadCount = ThreadCount + 1 #increment threadcount
+    else: #otherwise, let the user know that the username was not valid.
+        conn.send("False".encode())
+        conn.close() #close the connection.
+s.close() #closes the socket.
 
 ####################################################################################################################################
 #Citation of External References and Templates Used
